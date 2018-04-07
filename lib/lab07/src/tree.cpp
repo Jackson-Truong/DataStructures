857d31dc7019988965ece4406096ac3d29e4926f
jacksontruong <jackson.truong@sjsu.edu>
2018-04-07 15:04:22 -0700

    Lab 7 tests are giving me errors but everything else is good Starting lab 7 each push is a new function added # Conflicts: #	lib/lab05/src/stack.cpp #	test/lab02_tests.cpp #	test/lab03_tests.cpp #	test/lab04_tests.cpp #	test/lab05_tests.cpp

--- lib/lab07/src/tree.cpp
+++ lib/lab07/src/tree.cpp
@@ -74,7 +74,29 @@ throw"ERROR, enter another key";
 
     // Print the path to the key, starting with root
     void tree::path_to(int key) {
-
+node* current = root;
+std::vector path;
+path.push_back(current->data);
+if(current->data == key){
+    std::cout<< path;
+}
+else{
+    while(current){
+        if(current->data > key){
+            path.push_back(current->data);
+            current = current->left;
+        }
+        else if(current->data <key){
+            path.push_back(current->data);
+            current = current->right;
+        }
+        else{
+            path.push_back(current->data);
+            std::cout<<path;
+        }
+    }
+}
+ throw"ERROR, check your key.";
     }
 
     // Number of items in the tree
